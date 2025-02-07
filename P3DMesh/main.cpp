fc283ac3e9ec71622ad710044e2b99042d8c80b0
Sailor <tskoepli@ncsu.edu>
2025-02-06 22:19:06 -0500

    Worked on plot3d converter

--- P3DMesh/main.cpp
+++ P3DMesh/main.cpp
@@ -1,52 +1,74 @@
 #include <stdio.h>
-
+#include <cmath>
 
 /// File for transforming Plot3d file format into structured coordinate list format
 
 int main(int argc, char *argv[]) {
 	
-	if (argc <= 1) {printf("Please provide file name.\n"); return 1;}
+	if (argc <= 1) {printf("Please provide input and output file name.\n"); return 1;}
 
-	FILE* fin = fopen(argv[1]);
+    printf("Reading in file: %s\n",argv[1]);
+
+	FILE* fin = fopen(argv[1],"r");
 
 	int nblock;
 	fscanf(fin,"%d",&nblock);
+    printf("Found %d blocks.\n",nblock);
 
 	if (nblock > 1) {printf("Code in multiblock support you lazy fool.\n"); return 1;}
 
 	int ni, nj, nk;
 	fscanf(fin,"%d %d %d",&ni, &nj, &nk);
+    printf("Block dim = (%d, %d, %d)\nProceeding to read in coordinate data.\n", ni, nj, nk);
+    if (nk > 1) {printf("\nWARNING\n!!!!!!!!!!!!!!!!!!!!\nThis is a 3D mesh!!!\n!!!!!!!!!!!!!!!!!!\n\n");}
 
 	int npoin = ni * nj * nk;
 	int nline = ceil(npoin * 0.25);
-	auto* x = (double*)malloc(npoin*sizeof(double));
-	auto* y = (double*)malloc(npoin*sizeof(double));
-	auto* z = (double*)malloc(npoin*sizeof(double));
+	auto* xyz = (double*)malloc(3*npoin*sizeof(double));
+	double *x, *y, *z;
 	
-	int ip=0;
-	for (int iline=0; iline<nline; iline++) {
+    double row[4]{-999.0};
+    int ind = 0;
+	while (true) {
+        int asdf = (fscanf(fin,"%e %e %e %e",&(row[0]),&(row[1]),&(row[2]),&(row[3])) >= 1);
+        if (asdf < 1) {break;}
+        //printf("%d\n",asdf);
+        //
+        for (int i=0; i<4; i++) {
+        //
+            if (ind+i < 3*npoin) {
+                xyz[ind+i] = row[i];
+            } else {
+                break;
+            }
+        }
+        //
+    }
 
-		if (iline == nline-1){
-	        int p1,p2,p3,p4;
-		while (ip < npoin){
-		
+    printf("Finished Reading in from file\n");
+    
+    x = xyz;
+    y = &xyz[npoin];
+    //z = &xyz[2*npoin];
 
-		}
-		
+    printf("Writing to File: %s\n", argv[2]);
+    FILE* fout = fopen(argv[2],"w");
+    fprintf(fout, "%d %d\n",ni,nj);
 
-		fscanf(fin,"%d %d %d %d",x[ip],x[ip+1],x[ip+2],x[ip+3]);
-		ip += 4;
-	}
-
-	for (int k=0,k<ni,k++) {
-	for (int j=0,j<nj,k++) {
-	for (int i=0,i<nk,k++) {
-
-		
-
-	}
-	}
-	}
-
+    for (int j=0; j<nj; j++) {
+    for (int i=0; i<ni; i++) {
+        int ind = nj*i + j; ///////////////////////////////////////////////////////////////////////////////////////
+        fprintf(fout,"%.15le %.15le\n", x[ind], y[ind]);
+    }
+    }
+        
+    fclose(fin);
+    fclose(fout);
 	return 0;
 }
+
+
+
+
+
+
