372dcd1df154b07a30637b72071baed24dbbd11c
Sailor <tskoepli@ncsu.edu>
2025-02-11 14:10:46 -0500

    update

---
+++ FV/cmake-debug/CMakeFiles/MPI2DFV.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MPI2DFV.dir/BoundaryConditions.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/BoundaryConditions.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/DGP1Tools.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/DGP1Tools.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/EulerFlux.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/EulerFlux.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/FileIO.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/FileIO.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/Jacobian.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/Jacobian.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/LUtools.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/LUtools.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/MeshModule.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/MeshModule.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/SpatialDiscretization.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/SpatialDiscretization.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/Thermo.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/Thermo.cpp.o.d"
+  "CMakeFiles/MPI2DFV.dir/main.cpp.o"
+  "CMakeFiles/MPI2DFV.dir/main.cpp.o.d"
+  "MPI2DFV"
+  "MPI2DFV.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MPI2DFV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
