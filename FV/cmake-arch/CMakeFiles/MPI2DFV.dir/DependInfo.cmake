70d2321a2430f7edafe74afbe266bd8f1d20450c
Sailor Koeplinger <40185227+MrPezser@users.noreply.github.com>
2025-05-02 01:13:00 -0400

    Reran ffstep p0 as axi so it starts and converges

---
+++ FV/cmake-arch/CMakeFiles/MPI2DFV.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tsk/Documents/GitHub/PAR2D/FV/BoundaryConditions.cpp" "CMakeFiles/MPI2DFV.dir/BoundaryConditions.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/BoundaryConditions.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/DGP1Tools.cpp" "CMakeFiles/MPI2DFV.dir/DGP1Tools.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/DGP1Tools.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/EulerFlux.cpp" "CMakeFiles/MPI2DFV.dir/EulerFlux.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/EulerFlux.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/FileIO.cpp" "CMakeFiles/MPI2DFV.dir/FileIO.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/FileIO.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/Jacobian.cpp" "CMakeFiles/MPI2DFV.dir/Jacobian.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/Jacobian.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/LUtools.cpp" "CMakeFiles/MPI2DFV.dir/LUtools.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/LUtools.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/MeshModule.cpp" "CMakeFiles/MPI2DFV.dir/MeshModule.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/MeshModule.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/SpatialDiscretization.cpp" "CMakeFiles/MPI2DFV.dir/SpatialDiscretization.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/SpatialDiscretization.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/Thermo.cpp" "CMakeFiles/MPI2DFV.dir/Thermo.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/Thermo.cpp.o.d"
+  "/home/tsk/Documents/GitHub/PAR2D/FV/main.cpp" "CMakeFiles/MPI2DFV.dir/main.cpp.o" "gcc" "CMakeFiles/MPI2DFV.dir/main.cpp.o.d"
+  "" "MPI2DFV" "gcc" "CMakeFiles/MPI2DFV.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FV/cmake-arch/CMakeFiles/MPI2DFV.dir/link.d
@@ -0,0 +1,172 @@
+MPI2DFV: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o \
+  CMakeFiles/MPI2DFV.dir/main.cpp.o \
+  CMakeFiles/MPI2DFV.dir/EulerFlux.cpp.o \
+  CMakeFiles/MPI2DFV.dir/BoundaryConditions.cpp.o \
+  CMakeFiles/MPI2DFV.dir/FileIO.cpp.o \
+  CMakeFiles/MPI2DFV.dir/SpatialDiscretization.cpp.o \
+  CMakeFiles/MPI2DFV.dir/MeshModule.cpp.o \
+  CMakeFiles/MPI2DFV.dir/LUtools.cpp.o \
+  CMakeFiles/MPI2DFV.dir/Jacobian.cpp.o \
+  CMakeFiles/MPI2DFV.dir/Thermo.cpp.o \
+  CMakeFiles/MPI2DFV.dir/DGP1Tools.cpp.o \
+  /usr/lib/libmpi.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o \
+  /usr/lib/libopen-pal.so.80 \
+  /usr/lib/libfabric.so.1 \
+  /usr/lib/libucp.so.0 \
+  /usr/lib/libucs.so.0 \
+  /usr/lib/libevent_core-2.1.so.7 \
+  /usr/lib/libevent_pthreads-2.1.so.7 \
+  /usr/lib/libhwloc.so.15 \
+  /usr/lib/libpmix.so.2 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/libucm.so.0 \
+  /usr/lib/libuct.so.0 \
+  /usr/lib/libnuma.so.1 \
+  /usr/lib/libsframe.so.1 \
+  /usr/lib/libz.so.1 \
+  /usr/lib/libzstd.so.1 \
+  /usr/lib/libudev.so.1 \
+  /usr/lib/libcap.so.2
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtbeginS.o:
+
+CMakeFiles/MPI2DFV.dir/main.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/EulerFlux.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/BoundaryConditions.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/FileIO.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/SpatialDiscretization.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/MeshModule.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/LUtools.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/Jacobian.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/Thermo.cpp.o:
+
+CMakeFiles/MPI2DFV.dir/DGP1Tools.cpp.o:
+
+/usr/lib/libmpi.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/../../../../lib/crtn.o:
+
+/usr/lib/libopen-pal.so.80:
+
+/usr/lib/libfabric.so.1:
+
+/usr/lib/libucp.so.0:
+
+/usr/lib/libucs.so.0:
+
+/usr/lib/libevent_core-2.1.so.7:
+
+/usr/lib/libevent_pthreads-2.1.so.7:
+
+/usr/lib/libhwloc.so.15:
+
+/usr/lib/libpmix.so.2:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/libucm.so.0:
+
+/usr/lib/libuct.so.0:
+
+/usr/lib/libnuma.so.1:
+
+/usr/lib/libsframe.so.1:
+
+/usr/lib/libz.so.1:
+
+/usr/lib/libzstd.so.1:
+
+/usr/lib/libudev.so.1:
+
+/usr/lib/libcap.so.2:
